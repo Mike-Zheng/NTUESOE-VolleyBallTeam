44e2d9444a8b13a8e9d2e7cfb32e7e2cae17a1aa
mike <liwenzheng.mike@gmail.com>
2016-11-16 17:20:47 +0800

    update 1

---
+++ prejs/initJs.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+
+    $(".color-1").on("click", function() {
+        $(".design").css("background-color", "rgb(231, 14, 0)");
+    });
+    $(".color-2").on("click", function() {
+        $(".design").css("background-color", "rgb(234, 227, 77)");
+    });
+    $(".color-3").on("click", function() {
+        $(".design").css("background-color", "#303136");
+    });
+    $(".color-4").on("click", function() {
+        $(".design").css("background-color", "#000000");
+    });
+    $(".color-5").on("click", function() {
+        $(".design").css("background-color", "rgb(255, 152, 50)");
+    });
+    $(".color-6").on("click", function() {
+        $(".design").css("background-color", "#c2e3f2");
+    });
+    $(".color-7").on("click", function() {
+        $(".design").css("background-color", "rgb(40, 94, 192)");
+    });
+    $(".color-8").on("click", function() {
+        $(".design").css("background-color", "#9F998D");
+    });
+    $(".color-9").on("click", function() {
+        $(".design").css("background-color", "#ffbbbb");
+    });
+    $(".color-10").on("click", function() {
+        $(".design").css("background-color", "#f2186c");
+    });
+    $(".color-11").on("click", function() {
+        $(".design").css("background-color", "#D0D96C");
+    });
+    $(".color-12").on("click", function() {
+        $(".design").css("background-color", "#322551");
+    });
+
+});
+
